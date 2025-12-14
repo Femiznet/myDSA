4a679d210dd8c2414edadfd048eea5427d6ebfc7
Femiznet <ajuwonfemi121@gmail.com>
2025-12-14 10:08:39 +0100

    Refactor ROMAN_INT_FUNCTION
    Refactored the function to convert Roman numerals to integers, added handling for equal numerals, and improved variable naming.

--- roman_to_int.py
+++ roman_to_int.py
@@ -1,28 +1,48 @@
-def rom(roman:str):
-    roman = roman.upper()
+#My First trial on converting roman numerals to integers
+
+#     CONVERTING ROMAN NUMERALS TO INTEGERS
+
+def roman_to_int(roman_num:str):
+    roman_num = roman_num.upper() 
     roman_dict = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
     
-    length_roman = len(roman)
-    idx = 0
-    result = 0
+    length_roman = len(roman_num)
+    indx = 0 # index of each numerals in the roman_num
+    result = 0 
 
-    while idx < length_roman:
-        init = roman_dict[roman[idx]]
-        idx_2 = idx+1
-        next = 0
-        if idx_2 <= length_roman-1:
-            next = roman_dict[roman[idx_2]]
+    # converting each roman_num index by index
+    while indx < length_roman:
+        current_num = roman_dict[roman_num[indx]] 
+        next_indx = indx+1 
+        nxt_num = 0 
 
-        if init < next:
-            result += (next - init)
-            idx+=2
+        # if there is a next index
+        if next_indx <= length_roman-1:
+            nxt_num = roman_dict[roman_num[next_indx]] 
+
+        # roman numeral calculations
+        if current_num < nxt_num:
+            result += (nxt_num - current_num)
+            indx+=2 # skip the two index worked with
+        elif current_num == nxt_num:
+            result += (current_num + nxt_num)
+            indx+=2
         else:
-            result+=init
-            idx+=1
+            result+=current_num
+            indx+=1 # Skip only one index if current_num > next_num
 
     return result
 
 
-
-
-        
+if __name__ == "__main__":
+    roman_numerals = ["I", "II", "III", "IV", "V"
+                      "X", "L", "C", "D", "M"]
+    
+    for num in roman_numerals:
+        rom_int = roman_to_int(num)
+        try:
+            assert rom_int in [1, 2, 3, 4, 5, 10, 50, 100, 500, 1000]
+            print (rom_int, end=" ")
+        except AssertionError:
+            print ("Wrong numeral for ", num)
+            break
